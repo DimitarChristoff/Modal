131e8c038c798997b0157e2cbb62ff36301cb749
DimitarChristoff <christoff@gmail.com>
2011-12-15 14:08:43 +0000

    refactor and more fallback options for CSS3 transitions

--- Source/js/Modal.js
+++ Source/js/Modal.js
@@ -5,11 +5,11 @@ name: Modal.bootStrap
 
 description: provides a dom boostrap for modal content
 
-authors: Daniel Buchner, Dimitar Christoff
+authors: Daniel Buchner, Dimitar Christoff, Simon Smith
 
 license: MIT-style license.
 
-version: 1
+version: 1.02
 
 requires:
   - Core/String
@@ -18,7 +18,7 @@ requires:
   - Core/Array
   - Core/Class
 
-provides: COMMON.Overlay, COMMON.Modal, COMMON.Modal.bootstrap
+provides: COMMON.Overlay, COMMON.Modal, COMMON.Modal.bootstrap, Modernizr
 
 ...
 */
@@ -28,7 +28,10 @@ provides: COMMON.Overlay, COMMON.Modal, COMMON.Modal.bootstrap
 this.COMMON = this.COMMON || {};
 
 //detect css3 transition support via modernizr if needed
-this.Modernizr = this.Modernizr || function(a,b,c){function z(a,b){var c=a.charAt(0).toUpperCase()+a.substr(1),d=(a+" "+m.join(c+" ")+c).split(" ");return y(d,b)}function y(a,b){for(var d in a)if(j[a[d]]!==c)return b=="pfx"?a[d]:!0;return!1}function x(a,b){return!!~(""+a).indexOf(b)}function w(a,b){return typeof a===b}function v(a,b){return u(prefixes.join(a+";")+(b||""))}function u(a){j.cssText=a}var d="2.0.6",e={},f=b.documentElement,g=b.head||b.getElementsByTagName("head")[0],h="modernizr",i=b.createElement(h),j=i.style,k,l=Object.prototype.toString,m="Webkit Moz O ms Khtml".split(" "),n={},o={},p={},q=[],r,s={}.hasOwnProperty,t;!w(s,c)&&!w(s.call,c)?t=function(a,b){return s.call(a,b)}:t=function(a,b){return b in a&&w(a.constructor.prototype[b],c)},n.csstransitions=function(){return z("transitionProperty")};for(var A in n)t(n,A)&&(r=A.toLowerCase(),e[r]=n[A](),q.push((e[r]?"":"no-")+r));u(""),i=k=null,e._version=d,e._domPrefixes=m,e.testProp=function(a){return y([a])},e.testAllProps=z;return e}(this,this.document);
+var Modernizr = this.Modernizr || function(a,b,c){function z(a,b){var c=a.charAt(0).toUpperCase()+a.substr(1),d=(a+" "+m.join(c+" ")+c).split(" ");return y(d,b)}function y(a,b){for(var d in a)if(j[a[d]]!==c)return b=="pfx"?a[d]:!0;return!1}function x(a,b){return!!~(""+a).indexOf(b)}function w(a,b){return typeof a===b}function v(a,b){return u(prefixes.join(a+";")+(b||""))}function u(a){j.cssText=a}var d="2.0.6",e={},f=b.documentElement,g=b.head||b.getElementsByTagName("head")[0],h="modernizr",i=b.createElement(h),j=i.style,k,l=Object.prototype.toString,m="Webkit Moz O ms Khtml".split(" "),n={},o={},p={},q=[],r,s={}.hasOwnProperty,t;!w(s,c)&&!w(s.call,c)?t=function(a,b){return s.call(a,b)}:t=function(a,b){return b in a&&w(a.constructor.prototype[b],c)},n.csstransitions=function(){return z("transitionProperty")};for(var A in n)t(n,A)&&(r=A.toLowerCase(),e[r]=n[A](),q.push((e[r]?"":"no-")+r));u(""),i=k=null,e._version=d,e._domPrefixes=m,e.testProp=function(a){return y([a])},e.testAllProps=z;return e}(this,this.document);
+
+// provides:
+// Modernizr.csstransitions = false;
 
 Element.implement({
     diffuse: function(position){
@@ -104,8 +107,10 @@ var Modal = this.COMMON.Modal = new Class({
         header: "",
         body: "",
         footer: "",
+        useTransitions: true && Modernizr.csstransitions,
         transitionClass: "transition",
-        transitionShowClass: "modalVisible",
+		transitionClassBox: "modal-box-initial",
+        transitionShowClass: "modal-visible",
         fx: {
             duration: 300,
             properties: {
@@ -156,12 +161,13 @@ var Modal = this.COMMON.Modal = new Class({
         // save references
         this.element = proxy.getFirst().store('modal', this).inject(this.container);
         this.box = this.element.getElement('div.modal-box').set('morph', this.options.fx).setStyles({
-            'opacity': 0,
-            'marginTop': 30
+            'opacity': 0
         });
 
-        if (Modernizr.csstransitions)
+        if (this.options.useTransitions) {
             this.box.addClass(this.options.transitionClass);
+			this.box.addClass(this.options.transitionClassBox);
+        }
 
         this.content = this.box.getElement('div.modal-content');
         this.body = this.content.getFirst();
@@ -235,11 +241,11 @@ var Modal = this.COMMON.Modal = new Class({
         if (options.overlay)
             this.overlay.show();
 
-        if (Modernizr.csstransitions) {
+        if (this.options.useTransitions) {
             this.box.addClass(this.options.transitionShowClass);
         }
         else {
-            this.box.setStyle('left', '100%').morph(options.fx.properties.show);
+            this.box.morph(options.fx.properties.show);
         }
         this.isShown = true;
         this.fireEvent('show');
@@ -248,11 +254,11 @@ var Modal = this.COMMON.Modal = new Class({
     },
 
     hide: function(){
-        if (Modernizr.csstransitions) {
+        if (this.options.useTransitions) {
             this.box.removeClass(this.options.transitionShowClass);
         }
         else {
-            this.box.morph(this.options.fx.properties.hide).get('morph').chain(function(){ this.element.setStyle('left', 0) });
+            this.box.morph(this.options.fx.properties.hide);
         }
         this.isShown = false;
         this.fireEvent('hide');
